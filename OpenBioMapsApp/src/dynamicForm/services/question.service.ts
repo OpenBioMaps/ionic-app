44d6e8084c4ae38b0504c33f19b92970e817406c
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2016-12-27 00:39:01 +0100

    json converter added

--- OpenBioMapsApp/src/dynamicForm/services/question.service.ts
+++ OpenBioMapsApp/src/dynamicForm/services/question.service.ts
@@ -1,40 +1,38 @@
 import { Injectable }       from '@angular/core';
-import { DropdownQuestion } from '../models/question-dropdown';
 import { QuestionBase }     from '../models/question-base';
-import { TextboxQuestion }  from '../models/question-textbox';
+import { JsonConverter }    from '../../utils/json-converter';
 
 @Injectable()
 export class QuestionService {
+  json: string = `[{
+      "key": "brave",
+      "questionType": "dropdown",
+      "label": "Bravery Rating",
+      "order": 3,
+      "options": [
+        {"key": "solid",  "value": "Solid"},
+        {"key": "great",  "value": "Great"},
+        {"key": "good",   "value": "Good"},
+        {"key": "unproven", "value": "Unproven"}
+      ]
+    }, {
+      "key": "firstName",
+      "questionType": "textbox",
+      "label": "First name",
+      "value": "Bombasto",
+      "required": true,
+      "order": 1
+    }, {
+      "key": "emailAddress",
+      "questionType": "textbox",
+      "label": "Email",
+      "type": "email",
+      "order": 2
+    }]`;
+
   // Todo: get from a remote source of question metadata
   getQuestions(): Promise<QuestionBase<any>[]> {
-    let questions: QuestionBase<any>[] = [
-      new DropdownQuestion({
-        key: 'brave',
-        label: 'Bravery Rating',
-        options: [
-          {key: 'solid',  value: 'Solid'},
-          {key: 'great',  value: 'Great'},
-          {key: 'good',   value: 'Good'},
-          {key: 'unproven', value: 'Unproven'}
-        ],
-        order: 3
-      }),
-
-      new TextboxQuestion({
-        key: 'firstName',
-        label: 'First name',
-        value: 'Bombasto',
-        required: true,
-        order: 1
-      }),
-
-      new TextboxQuestion({
-        key: 'emailAddress',
-        label: 'Email',
-        type: 'email',
-        order: 2
-      })
-    ];
+    let questions: QuestionBase<any>[] = JsonConverter.convert(this.json);
 
     return Promise.resolve(questions.sort((a, b) => a.order - b.order));
   }
---
+++ OpenBioMapsApp/src/utils/json-converter.ts
@@ -0,0 +1,30 @@
+import { QuestionBase }     from '../dynamicForm/models/question-base';
+import { DropdownQuestion } from '../dynamicForm/models/question-dropdown';
+import { TextboxQuestion }  from '../dynamicForm/models/question-textbox';
+
+
+export class JsonConverter {
+
+  // TODO Handle invalid json
+  static convert(jsonStr: string): QuestionBase<any>[] {
+    let json: any[] = JSON.parse(jsonStr);
+    let questions: QuestionBase<any>[] = [];
+
+    json.forEach(object => {
+      let question: QuestionBase<any>;
+      switch (object.questionType) {
+        case 'textbox':
+          question = new TextboxQuestion(object);
+          break;
+        case 'dropdown':
+          question = new DropdownQuestion(object);
+          break;
+        default:
+
+      }
+      questions.push(question);
+    });
+
+    return questions;
+  }
+}
