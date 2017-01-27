8ca3a40903006f17035ed348c5937ad7b79d1a3f
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-01-28 00:44:20 +0100

    Redesign and refactor settings page

--- OpenBioMapsApp/src/pages/settings/settings.ts
+++ OpenBioMapsApp/src/pages/settings/settings.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.html',
-  providers:  [StorageService]
+  providers:  [ StorageService ]
 })
 export class Settings {
   settingsForm: FormGroup;
@@ -15,14 +15,26 @@ export class Settings {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private storage: StorageService,
-              private formBuilder: FormBuilder ) {
-  this.settingsForm = this.formBuilder.group({
-                  url: ['', Validators.required]
-                });
+              private formBuilder: FormBuilder,
+              private toastCtrl: ToastController ) {
+    this.settingsForm = this.formBuilder
+      .group({
+        url: ['', Validators.required]
+      });
     this.storage.getUrl().then(url => this.settingsForm.controls['url'].setValue(url));
   }
 
   saveSettings() {
-    this.storage.setUrl(this.settingsForm.get('url').value);
+    this.storage
+      .setUrl(this.settingsForm.get('url').value)
+      .then(() => this.showSavedToast());
+  }
+
+  showSavedToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Settings saved',
+      duration: 3000
+    });
+    toast.present();
   }
 }
