ebe5558157ef6309f7b1f158324924ae4d82c635
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-02-13 21:49:17 +0100

    Upload service implementation started

--- OpenBioMapsApp/src/pages/dynamicForm/dynamicForm.ts
+++ OpenBioMapsApp/src/pages/dynamicForm/dynamicForm.ts
@@ -3,16 +3,20 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { QuestionService } from '../../dynamicForm/services/question.service';
+import { UploadService } from '../../services/upload.service';
 
 @Component({
   selector: 'page-dynamic-form',
   templateUrl: 'dynamicForm.html',
-  providers:  [QuestionService]
+  providers:  [QuestionService, UploadService]
 })
 export class DynamicForm {
   questions: any[];
 
-  constructor(public navCtrl: NavController, service: QuestionService) {
-    service.getQuestionsSlowly().then(questions => this.questions = questions);;
+  constructor(public navCtrl: NavController,
+    private questionService: QuestionService,
+    private uploadService: UploadService) {
+    this.questionService.getQuestionsSlowly().then(questions => this.questions = questions);
+    this.uploadService.upload();
   }
 }
