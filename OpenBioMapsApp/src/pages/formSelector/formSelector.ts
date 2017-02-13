17f8bf60fab2aec13fdfe69cbedc3f7b647bda05
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-02-13 23:35:54 +0100

    Add basic api service

--- OpenBioMapsApp/src/pages/formSelector/formSelector.ts
+++ OpenBioMapsApp/src/pages/formSelector/formSelector.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { DynamicForm } from '../dynamicForm/dynamicForm';
-import { StorageService } from '../../services/storage.service';
+import { ObmApiService } from '../../services/obmApi.service';
 import { RepoService } from '../../services/repo.service';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'page-form-selector',
   templateUrl: 'formSelector.html',
-  providers:  [ RepoService, StorageService ]
+  providers:  [ ObmApiService, RepoService, StorageService ]
 })
 export class FormSelector {
   error: any;
@@ -18,14 +19,14 @@ export class FormSelector {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private repo: RepoService,
+              private api: ObmApiService,
               private storage: StorageService ) {
     this.storage
       .getSettings()
       .then(settings => {
         if(settings) {
           this.url = settings.url;
-          return this.repo.loadUrl(this.url);
+          return this.api.loadFormList(this.url)
         } else {
           // TODO Display no url set warning
         }
---
+++ OpenBioMapsApp/src/services/obmApi.service.ts
@@ -0,0 +1,25 @@
+import { Injectable }   from '@angular/core';
+import { Http }         from '@angular/http';
+
+import { RepoService } from './repo.service';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ObmApiService {
+  private static readonly GET_FORMS = 'get_api_forms';
+  private static readonly GET_FORM = 'get_api_form';
+
+  constructor(private repo: RepoService) {}
+
+  loadFormList(url: string): Promise<any> {
+    url += '&' + ObmApiService.GET_FORMS;
+    return this.repo.loadUrl(url);
+  }
+
+  loadForm(url: string, id: number): Promise<any> {
+    url += '&' + ObmApiService.GET_FORM + '=' + id.toString();
+    return this.repo.loadUrl(url);
+  }
+}
