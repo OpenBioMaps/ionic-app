97394894cc797b9c6ec8988628f3ed77f578a489
Csaba Szugyiczki <szugyiczkicsaba@gmail.com>
2017-02-20 23:52:58 +0100

    Add basic json error handling

--- OpenBioMapsApp/src/utils/json-converter.ts
+++ OpenBioMapsApp/src/utils/json-converter.ts
@@ -6,10 +6,14 @@ import { TextboxQuestion }  from '../dynamicForm/models/question-textbox';
 export class JsonConverter {
 
   // TODO Handle invalid json
+  // TODO Remove logs
   static convert(json: any[]): QuestionBase<any>[] {
     let questions: QuestionBase<any>[] = [];
+    console.log(json);
 
     json.forEach(object => {
+      console.log(object);
+
       let question: QuestionBase<any>;
       switch (object.questionType) {
         case 'textbox':
@@ -19,11 +23,14 @@ export class JsonConverter {
           question = new DropdownQuestion(object);
           break;
         default:
-
+          // TODO Add default question model
+          question = new QuestionBase<any>();
+          break;
       }
       questions.push(question);
     });
 
+    console.log(questions);
     return questions;
   }
 }
