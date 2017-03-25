651eb640f50ce3d767b3ab26508e51083155e26f
Viktória Kurják <viktorijaa95@hotmail.com>
2017-03-25 15:00:38 +0100

    Numeric inputs updated, get geolocation button added

--- OpenBioMapsApp/src/dynamicForm/models/question-textbox.ts
+++ OpenBioMapsApp/src/dynamicForm/models/question-textbox.ts
@@ -2,10 +2,8 @@ import { QuestionBase } from './question-base';
 
 export class TextboxQuestion extends QuestionBase<string> {
   type = 'text';
-  controlType: string;
 
   constructor(options: {} = {}) {
     super(options);
-    this.controlType = options['controlType'] || '';
   }
 }
