e629173928902e18c65c0d034345ef5c969cea11
Viktória Kurják <viktorijaa95@hotmail.com>
2017-05-12 16:27:38 +0200

    GPS state + getGeolocation

--- OpenBioMapsApp/src/dynamicForm/dynamic-form-question.component.ts
+++ OpenBioMapsApp/src/dynamicForm/dynamic-form-question.component.ts
@@ -1,41 +1,51 @@
 import { Component, Input } from '@angular/core';
-import { FormGroup }        from '@angular/forms';
-import { QuestionBase }     from './models/question-base';
+import { FormGroup } from '@angular/forms';
+import { QuestionBase } from './models/question-base';
 import { Geolocation } from '@ionic-native/geolocation';
-import { AlertController } from  'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Diagnostic } from '@ionic-native/diagnostic';
 
 @Component({
   selector: 'df-question',
   templateUrl: 'dynamic-form-question.component.html',
-  providers: [ Geolocation ]
+  providers: [Geolocation, Diagnostic]
 })
 
 export class DynamicFormQuestionComponent {
   @Input() question: QuestionBase<any>;
   @Input() form: FormGroup;
   val: string = null;
-  
+
 
   constructor(private geolocation: Geolocation,
-              private alertCtrl: AlertController) { }
-              
+    private alertCtrl: AlertController,
+    private diagnostic: Diagnostic) { }
+
 
   get isValid() { return this.form.controls[this.question.key].valid; }
   public hasError(key: string) { return this.form.controls[this.question.key].hasError(key); }
 
-  getGeolocation(){
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.val = resp.coords.latitude.toString() + ',' +  resp.coords.longitude.toString();
-      }).catch((error) => {
-        //console.log('Error getting location', error);
-        var alert = this.alertCtrl.create({
-        title: 'Error on GPS',
-        subTitle: 'You need active the GPS',
-        buttons: ['Ok']
+  getGeolocation() {
+    this.diagnostic.isGpsLocationAvailable().then((result) => {
+      if (result) {
+        this.geolocation.getCurrentPosition().then((resp) => {
+          console.log("koordináták:" + resp.coords.latitude.toString() + ',' + resp.coords.longitude.toString());
+          this.val = resp.coords.latitude.toString() + ',' + resp.coords.longitude.toString();
+        })
+      }
+      else {
+        let alert = this.alertCtrl.create({
+          title: 'GPS is disabled',
+          message: 'GPS is disabled on your device. Please check your settings!',
+          buttons: [{
+            text: 'OK',
+            handler: () => {
+              this.diagnostic.switchToLocationSettings();
+            }
+          }]
         });
-      alert.present();
-    });
-    
+        alert.present();
+      }
+    })
   }
-  
 }
